d4b0ef6da1511ee64c100d2ff147098b4c49ad3e
sanukumar <sanukumar1138@gmail.com>
2022-07-25 22:12:44 +0530

    updated key

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import store from './app/store';
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom"; // In order to use links we need to wrap our app with react-router.
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
-  document.getElementById('root')
+import App from "./App";
+import store from "./app/store";
+import "antd/dist/antd.css";
+
+ReactDom.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+    ,
+  </React.StrictMode>,
+  document.getElementById("root")
 );
